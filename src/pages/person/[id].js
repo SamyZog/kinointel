15e2afcb4401301f3a1d1cecf6857eec045d9be1
Samy Zogeyb <samyzogeyb@gmail.com>
2021-07-01 11:27:25 +0400

    figuring out deploy error

--- src/pages/person/[id].js
+++ src/pages/person/[id].js
@@ -33,7 +33,7 @@ export default Person;
 export async function getStaticPaths(context) {
 	// too manny paths to pre-render, we set fallback to true https://nextjs.org/docs/basic-features/data-fetching#when-is-fallback-true-useful
 	// used fallback: true over fallback: "blocking" to load the page faster from the user's perspective https://nextjs.org/docs/basic-features/data-fetching#fallback-blocking
-	return { paths: [{ params: { id: "" } }], fallback: true };
+	return { paths: [{ params: { id: "id" } }], fallback: true };
 }
 
 export async function getStaticProps(context) {
